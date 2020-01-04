00e33c64d55b1cd642aaea2d676123abfe3d3b9a
liuhongyin <liuhongyin7761@gmail.com>
2020-01-04 20:56:19 +0800

    add jwt dependency

---
+++ src/main/java/com/liuhongyin/springsecurity/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.liuhongyin.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author marcot
+ * @since 1/4/20
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "test";
+    }
+}
